3a14aa5322c12cb68d06ba77018eac6b62576755
Santiago Chiquiza <eschiquizac@academia.usbbog.edu.co>
2025-08-25 16:28:30 -0500

    Update reto3.py

--- python_retos/reto3_buscar_booleans/reto3.py
+++ python_retos/reto3_buscar_booleans/reto3.py
@@ -7,7 +7,7 @@
 
 import re
 
-texto = "La respuesta es True, pero a veces es False."
+texto = "pebne."
 
 # Expresión regular para booleanos (True o False, case-insensitive)
 patron = r"\b(True|False)\b"
